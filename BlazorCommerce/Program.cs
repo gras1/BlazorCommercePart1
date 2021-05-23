7f6c1f68dd7a23eb09243885eabff21e3a301197
Graham Fearn <graham@fearnmarketingservices.com>
2021-05-23 19:36:47 +0100

    Reverted repo so that all steps up to 108 have been completed

--- BlazorCommerce/Program.cs
+++ BlazorCommerce/Program.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace BlazorCommerce
 {
@@ -14,8 +18,9 @@ namespace BlazorCommerce
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5005/")});
-            builder.Services.AddScoped<AppState>();
 
+            builder.Services.AddScoped<AppState>();
+            
             await builder.Build().RunAsync();
         }
     }
